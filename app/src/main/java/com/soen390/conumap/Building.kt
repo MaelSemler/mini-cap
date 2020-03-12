152c6d08e556321d286542d4fc16a991a0955803
lposo <lposo26@users.noreply.github.com>
2020-03-12 13:51:05 -0400

    Began work on addShape in Building.kt
    Added shape function in Building.kt, doesn't work yet/

--- app/src/main/java/com/soen390/conumap/Building.kt
+++ app/src/main/java/com/soen390/conumap/Building.kt
@@ -1,7 +1,10 @@
 package com.soen390.conumap
 
+import android.graphics.Color
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolygonOptions
 
 class Building {
     var name: String
@@ -9,6 +12,7 @@ class Building {
     var location: LatLng
     var marker: Marker
     var touchTarget: Polygon
+    var coordinateArray: Array<String>
 
     constructor(name: String, info: String, location: LatLng, map: GoogleMap) {
         this.name = name
@@ -16,7 +20,18 @@ class Building {
         this.location = location
         this.marker = addBuildingMarker(map)
         this.touchTarget = addTouchTarget(map)
+        this.coordinateArray = arrayOf<String>()
     }
+
+    constructor(name: String, info: String, location: LatLng, map: GoogleMap, coordinateArray: Array<String>) {
+        this.name = name
+        this.info = info
+        this.location = location
+        this.marker = addBuildingMarker(map)
+        this.touchTarget = addTouchTarget(map)
+        this.coordinateArray = coordinateArray
+    }
+
 
     // Adds marker for this building to the map passed as an argument.
     fun addBuildingMarker(map: GoogleMap): Marker {
@@ -36,5 +51,18 @@ class Building {
         )
     }
 
-
+    fun generatePolygonShape(): PolygonOptions{
+        val concordiaRed = Color.rgb(147,35,57)
+        var polygon = PolygonOptions()
+        for (i in coordinateArray.indices) {
+            if (i % 2 == 0) {
+                var latitude = coordinateArray[i].toDouble()
+                var longitude = coordinateArray[i + 1].toDouble()
+                var latlng = LatLng(latitude, longitude)
+                polygon.add(latlng)
+            }
+        }
+        polygon.fillColor(concordiaRed).strokeWidth(0.1f)
+        return polygon
+    }
 }
