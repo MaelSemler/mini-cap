b07f42aacf88448bb123a77dc89244577f4fef70
Stromation <leamsem719@hotmail.ca>
2020-03-12 16:36:46 -0400

    Changed the building class to have a primary constructor

--- app/src/main/java/com/soen390/conumap/Building/Building.kt
+++ app/src/main/java/com/soen390/conumap/Building/Building.kt
@@ -3,20 +3,14 @@ package com.soen390.conumap.Building
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.*
 
-class Building {
-        var name: String
-        var info: String
-        var location: LatLng
-        var marker: Marker
+class Building(var name: String, var info: String, var location: LatLng, map: GoogleMap) {
+    var marker: Marker
         var touchTarget: Polygon
 
-        constructor(name: String, info: String, location: LatLng, map: GoogleMap) {
-            this.name = name
-            this.info = info
-            this.location = location
-            this.marker = addBuildingMarker(map)
-            this.touchTarget = addTouchTarget(map)
-        }
+    init {
+        this.marker = addBuildingMarker(map)
+        this.touchTarget = addTouchTarget(map)
+    }
 
         // Adds marker for this building to the map passed as an argument.
         fun addBuildingMarker(map: GoogleMap): Marker {
