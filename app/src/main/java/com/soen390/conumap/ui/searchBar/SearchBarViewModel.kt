d4cc8fa60b5a717b4cc3242ee409e32790b36857
nguyhh <nguyenhaiha1224@gmail.com>
2020-03-12 16:14:02 -0400

    Add SearchBar

---
+++ app/src/main/java/com/soen390/conumap/ui/searchBar/SearchBarViewModel.kt
@@ -0,0 +1,7 @@
+package com.soen390.conumap.ui.searchBar
+
+import androidx.lifecycle.ViewModel
+
+class SearchBarViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/soen390/conumap/ui/searchBar/SearchCompletedViewModel.kt
@@ -0,0 +1,7 @@
+package com.soen390.conumap.ui.searchBar
+
+import androidx.lifecycle.ViewModel
+
+class SearchCompletedViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
