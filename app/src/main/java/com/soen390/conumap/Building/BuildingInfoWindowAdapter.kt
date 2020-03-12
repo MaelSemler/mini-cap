4c7c3d99df634b323dad4e514521dc93f41d4046
nguyhh <nguyenhaiha1224@gmail.com>
2020-03-12 15:55:09 -0400

    Add Building class and BuildingInfoWindowAdapter.kt

---
+++ app/src/main/java/com/soen390/conumap/Building/BuildingInfoWindowAdapter.kt
@@ -0,0 +1,44 @@
+package com.soen390.conumap.Building
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.soen390.conumap.R
+
+class BuildingInfoWindowAdapter : GoogleMap.InfoWindowAdapter{
+
+    private val view: View
+    private val context: Context
+
+    constructor(ctx: Context) {
+        context = ctx
+        view = LayoutInflater.from(context).inflate(R.layout.building_info_window, null)
+    }
+
+    private fun renderText(mkr: Marker, vw: View) {
+        var title = mkr.title
+        var buildingName = vw.findViewById<TextView>(R.id.building_name)
+        buildingName.text = title
+
+        var snippet = mkr.snippet
+        var buildingDetails = vw.findViewById<TextView>(R.id.building_details)
+        buildingDetails.text = snippet
+    }
+
+    override fun getInfoContents(mkr: Marker?): View? {
+        if (mkr != null) {
+            renderText(mkr, view)
+        }
+        return view
+    }
+
+    override fun getInfoWindow(mkr: Marker?): View? {
+        if (mkr != null) {
+            renderText(mkr, view)
+        }
+        return view
+    }
+}
