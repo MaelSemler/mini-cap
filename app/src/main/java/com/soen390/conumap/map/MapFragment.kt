3cd53e4168a79610d2b0ef09177a4175b3058f9e
Stromation <leamsem719@hotmail.ca>
2020-03-12 15:22:25 -0400

    Added MapFragment

---
+++ app/src/main/java/com/soen390/conumap/map/MapFragment.kt
@@ -0,0 +1,33 @@
+package com.soen390.conumap.map
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.soen390.conumap.R
+
+class MapFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MapFragment()
+    }
+
+    private lateinit var viewModel: MapViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.map_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MapViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
