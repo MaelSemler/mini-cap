6bf8b42825db3cb506d7b3145562cbbbdbd2e6ff
David Semujanga <dav.semu@gmail.com>
2020-03-13 12:31:22 -0400

    Modified the "SearchBar" front end only and navigation

--- app/src/main/java/com/soen390/conumap/ui/search_bar/SearchBarFragment.kt
+++ app/src/main/java/com/soen390/conumap/ui/search_bar/SearchBarFragment.kt
@@ -2,6 +2,7 @@ package com.soen390.conumap.ui.search_bar
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,11 +24,17 @@ class SearchBarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.search_bar_fragment, container, false)
-        //TODO: modify so that when the text search bar is selected, then change fragment
-        val search_button = root.findViewById<View>(R.id.search_input_button)
-        search_button.setOnClickListener{
-            NavHostFragment.findNavController(this).navigate(R.id.action_searchBarFragment_to_searchResultsFragment)
-        }
+
+        val search_bar = root.findViewById<View>(R.id.edSearch)
+        search_bar.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                NavHostFragment.findNavController(this).navigate(R.id.action_searchBarFragment_to_searchResultsFragment)
+            }
+            false
+        })
+
+
+
         return root
     }
 
