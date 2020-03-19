ab09f1b22075d3640e8c7c8e12daab43f5eaaad5
Johnston Stott <johnstonstott@gmail.com>
2020-03-19 18:18:51 -0400

    Working for API level 23

--- app/src/main/java/com/soen390/conumap/helper/DeviceLocationChecker.kt
+++ app/src/main/java/com/soen390/conumap/helper/DeviceLocationChecker.kt
@@ -6,18 +6,21 @@ import android.location.LocationManager
 import android.os.Build
 import android.provider.Settings
 import androidx.annotation.RequiresApi
+import kotlin.properties.Delegates
 
 // An object used to check if the location service on the device is enabled.
 object DeviceLocationChecker {
     private lateinit var context: Context
     fun setContext(ctx: Context) { context = ctx
         locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        mode = Settings.Secure.getInt(context.contentResolver, Settings.Secure.LOCATION_MODE, Settings.Secure.LOCATION_MODE_OFF)
     }
 
+    var mode = -1
     lateinit var locationManager: LocationManager
 
     @RequiresApi(Build.VERSION_CODES.P)
-    fun isDeviceLocationEnabled(): Boolean {
-        return locationManager.isLocationEnabled()
+    fun isDeviceLocationEnabled(): Int {
+        return mode
     }
 }
