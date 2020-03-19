8a768a19f92b77058e1ee0d1b000cd663e6293ef
David Semujanga <dav.semu@gmail.com>
2020-03-19 19:32:21 -0400

    Calendar Fragment and added menu layout

---
+++ app/src/main/java/com/soen390/conumap/ui/calendar/CalendarFragment.kt
@@ -0,0 +1,33 @@
+package com.soen390.conumap.ui.calendar
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.soen390.conumap.R
+
+class CalendarFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CalendarFragment()
+    }
+
+    private lateinit var viewModel: CalendarViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.calendar_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CalendarViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
