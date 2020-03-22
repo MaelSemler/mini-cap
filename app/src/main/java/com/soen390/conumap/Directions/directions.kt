084e0c6e6f634910ab92326ce220c0074d040669
nguyhh <nguyenhaiha1224@gmail.com>
2020-03-21 20:41:18 -0400

    Comments added

--- app/src/main/java/com/soen390/conumap/Directions/directions.kt
+++ app/src/main/java/com/soen390/conumap/Directions/directions.kt
@@ -47,21 +47,22 @@ object directions {
         //TODO: Set Transportation mode
         //This can be walking, driving, bicycling, transit
         //val transportationMode = getTransportationMode()
-        val transportationMode = "walking"
+        val transportationMode = "driving"//TODO: Hardcoded for now look at the line just before we should be able to retrieve it this way
 
         //TODO: Set Alternative On/Off
         //val alternativesOn = getAlternatives()
-        val alternativesOn = false
+        val alternativesOn = false//TODO: Hardcoded that alternatives are turned off for now look at line right before: to be implemented
 
        //TODO: Origin and Destination should have a title
         map.addMarker(originLatLng,("This is the origin"))
         map.addMarker(destinationLatLng, "Destination")
         map.moveCamera(originLatLng, 14.5f)
 
-        route(activity,originLatLng, destinationLatLng, transportationMode, alternativesOn)
+        route(activity,originLatLng, destinationLatLng, transportationMode, alternativesOn)//Calling the actual route function and passing all the needed parameters
     }
 
     //Put the correct parameters inside the api call
+    //This function returns the URL to make the GoogleMap API call
     private fun getGoogleMapRequestURL(activity: Activity, originLatLng: LatLng, destinationLatLng: LatLng, transportationMode: String, alternativesOn:Boolean):String{
         if(alternativesOn)
             return activity.getString(R.string.DirectionAPI)+ originLatLng.latitude + "," + originLatLng.longitude + "&destination=" + destinationLatLng.latitude + "," + destinationLatLng.longitude + "&mode=" + transportationMode + "&alternative=true"+ "&key=" + activity.getString(R.string.apiKey)
@@ -70,16 +71,21 @@ object directions {
     }
 
 
+    //Route methods which makes the call get the response from Google Directions API and parse the JSON files to store everything inside arrays
     private fun route(activity: Activity, originLatLng: LatLng, destinationLatLng: LatLng, transportationMode: String, alternativesOn: Boolean) {
+        //Path is an arrayList that store every "steps"/path =>Will be used to draw the path
         val path: MutableList<List<LatLng>> = ArrayList()
+
+        //Retrieve the correct URL to call the API
         val urlDirections = getGoogleMapRequestURL(activity, originLatLng, destinationLatLng, transportationMode, alternativesOn)
 
+        //Making the Request
         val directionsRequest = object : StringRequest(
             Request.Method.GET,
             urlDirections,
             com.android.volley.Response.Listener<String> { response ->
                 val jsonResponse = JSONObject(response)
-                // Get routes
+                // This part to understand it look carefully at the JSON response sent by the API
                 val routes = jsonResponse.getJSONArray("routes")
                 val legs = routes.getJSONObject(0).getJSONArray("legs")
                 val steps = legs.getJSONObject(0).getJSONArray("steps")
@@ -87,9 +93,10 @@ object directions {
                 val totalDistance =legs.getJSONObject(0).getJSONObject("distance").getString("text")
                 val totalDuration= legs.getJSONObject(0).getJSONObject("duration").getString("text")
 
-                //Clean up the directions
+                //ExtractDirections and save it into the directionText blocks
                 updateSteps(extractDirections(steps))
 
+                //Draw the path in path in red color
                 for (i in 0 until steps.length()) {
                     val points =
                         steps.getJSONObject(i).getJSONObject("polyline").getString("points")
@@ -104,12 +111,12 @@ object directions {
         val requestQueue = Volley.newRequestQueue(activity)
         requestQueue.add(directionsRequest)
 
-//        map.getMapInstance().setOnInfoWindowClickListener()
-
+        //Move the camera and zoom into the destination
         map.moveCamera(destinationLatLng, 18f)
     }
 
 
+    //From the JSON response, extract all needed informations such as direction(instruction), the distance and the duration
     fun extractDirections(steps: JSONArray): String {
         var textConverted = "Direction:" + '\n'
 
@@ -124,21 +131,33 @@ object directions {
             durationArray.add(steps.getJSONObject(i).getJSONObject("duration").getString("text"))
 
             var tempText = steps.getJSONObject(i).getString("html_instructions")
-            while(tempText.contains('<',true)){
-                val leftBracketIndex = tempText.indexOf('<')
-                val rightBracketIndex = tempText.indexOf('>')
-                tempText= tempText.removeRange(leftBracketIndex,rightBracketIndex+1)
-            }
-            directionArray.add(tempText)
+
+            directionArray.add(cleanUpDirections(tempText))
         }
 
+        //Loop through everything retrieved and make it into a pretty displayable block text
         for(i in 0 until distanceArray.size){
             textConverted += (i + 1).toString() + ". " +  directionArray[i] + '\n' + '\t' + distanceArray[i] +'\n'
         }
-        //Put everything inside of the directionTextBox
+
+        //return the appropriately formatted directions instruction block text
         return textConverted
     }
 
+    //Function to get rid of all HTML tags from the direction instructions
+    //return the instruction cleared of HTML tags
+    fun cleanUpDirections(tempText:String):String{
+        var tempText = tempText
+
+        while(tempText.contains('<',true)){
+            val leftBracketIndex = tempText.indexOf('<')
+            val rightBracketIndex = tempText.indexOf('>')
+            tempText= tempText.removeRange(leftBracketIndex,rightBracketIndex+1)
+        }
+        return tempText
+    }
+
+    //Update the directionText
     fun updateSteps(textSteps:String){
         _directionText.value = textSteps
     }
