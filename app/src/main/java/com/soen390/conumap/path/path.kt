eccd97f3162f1af0bf20af1deda645c51f8a0ecf
yabdelkawy <yabdelkawy@outlook.com>
2020-03-22 03:46:40 -0400

    Added a path class and starting working on alternate transporations

---
+++ app/src/main/java/com/soen390/conumap/path/path.kt
@@ -0,0 +1,34 @@
+package com.soen390.conumap.path
+
+import android.location.Location
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.maps.model.LatLng
+import com.soen390.conumap.Directions.directions
+
+
+//TODO: Implement Live Data Binding as in directions.kt
+object path {
+    //origin
+    val destination= Location()
+    private var distance=0.0
+    private var transportationMethod="Car"
+    //time
+    //accessibilityFriendly
+
+    init
+    {
+        //TODO: Initialize once data binding is done
+    }
+
+    fun switchOriginAndDestination()
+    {
+        //TODO: To be implemented
+    }
+
+    fun getAlternatives()
+    {
+        //TODO: To be implemented
+    }
+
+}
