486e5ba44aa323c92eb461a0194ea237f15af6a9
Andy <andypqvu@hotmail.com>
2020-03-27 18:35:28 -0400

    Updated PathFindingSampleTest with a slightly more challenging test.

--- app/src/main/java/com/soen390/conumap/IndoorNavigation/PathFindingSampleTest.kt
+++ app/src/main/java/com/soen390/conumap/IndoorNavigation/PathFindingSampleTest.kt
@@ -4,12 +4,12 @@ import java.nio.file.Path
 
 fun main (args: Array<String>) {
     var origin: Node = Node(2, 1)
-    var destination: Node = Node(2, 5)
-    var row = 6
-    var col = 7
+    var destination: Node = Node(7, 7)
+    var row = 8
+    var col = 8
     var pathfindingTest: Pathfinding = Pathfinding(row, col, origin, destination)
-    var blockRow = arrayOf(1,2,3)
-    var blockCol = arrayOf(3,3,3)
+    var blockRow = arrayOf(1,2,3,4,5,6,7,7,6,5,3,3,3)
+    var blockCol = arrayOf(3,3,3,3,3,3,3,6,6,6,5,6,7)
     var blockArray = arrayOf<Array<Int>>(blockRow, blockCol)
     pathfindingTest.loadMap()
     pathfindingTest.loadBlocks(blockArray)
