06ec6c32c0a35bfacd6241577f99e7f87daae9b9
Johnston Stott <johnstonstott@gmail.com>
2020-03-26 22:58:27 -0400

    Add building constants for length and width of buildings. Will be used for displaying indoor paths. This will be expanded as more buildings are supported for indoor directions.

--- app/src/main/java/com/soen390/conumap/building/BuildingsConstants.kt
+++ app/src/main/java/com/soen390/conumap/building/BuildingsConstants.kt
@@ -1,7 +1,9 @@
 package com.soen390.conumap.building
 
-import com.google.android.gms.maps.model.LatLng
-
 object BuildingsConstants {
-    val virus = LatLng(45.495506, -73.577774)
+    // Length and width of buildings determined by calculating distance between LatLngs.
+    val lengthHorH: Double = 0.000745
+    val lengthVerH: Double = 0.000776
+    val lengthHorMB: Double = 0.000368
+    val lengthVerMB: Double = 0.000761
 }
