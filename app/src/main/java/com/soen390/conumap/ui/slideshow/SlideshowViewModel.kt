a222c6144475eea63443b4a1f60dcbc664c534ff
Johnston Stott <johnstonstott@gmail.com>
2020-03-29 23:40:46 -0400

    Change labels of navigation menu to match mockups

--- app/src/main/java/com/soen390/conumap/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/com/soen390/conumap/ui/slideshow/SlideshowViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Settings"
     }
     val text: LiveData<String> = _text
 }
