78c313b37a443eb078c766f21533afde447d112e
nguyhh <nguyenhaiha1224@gmail.com>
2020-04-12 00:13:47 -0400

    Created setupPathPaint() method to initialize the Paintobject to draw path on indoor map

--- app/src/main/java/com/soen390/conumap/SVGConverter/FloorPlanTransformation.kt
+++ app/src/main/java/com/soen390/conumap/SVGConverter/FloorPlanTransformation.kt
@@ -12,21 +12,30 @@ class FloorPlanTransformation: Transformation {
 
     private lateinit var context:Context
     var svgCon: SvgConverter = SvgConverter
+    lateinit var pathPaint: Paint
 
     init {
         context = svgCon.getContext()
+        pathPaint = setupPathPaint()
+
+
     }
 
     private var mColor = 0
-
-    fun ColorFilterTransformation(color: Int) {
-        mColor = color
-    }
 
     var rs : RenderScript = RenderScript.create(context)
 
     override fun key(): String {
        return "Transformed floorPlan"
+    }
+
+    private fun setupPathPaint(): Paint{
+        pathPaint = Paint()
+        pathPaint.isAntiAlias = true
+        pathPaint.style = Paint.Style.STROKE
+        pathPaint.strokeWidth = 20.0F
+        pathPaint.color = Color.BLUE
+        return pathPaint
     }
 
     override fun transform(source: Bitmap): Bitmap {
@@ -37,41 +46,19 @@ class FloorPlanTransformation: Transformation {
 
         var canvas = Canvas(bitmap)
         var paint = Paint()
-        var paint2 = Paint()
-
-        paint2.setAntiAlias(true)
-        paint2.setStyle(Paint.Style.STROKE)
-        paint2.setStrokeWidth(20.0F)
-        paint2.setColor(Color.BLUE)
         paint.setAntiAlias(true)
-        this.ColorFilterTransformation(0)
+
         paint.setColorFilter(PorterDuffColorFilter(mColor, PorterDuff.Mode.SRC_ATOP))
         canvas.drawBitmap(source, 0.0F, 0.0F, paint)
-        canvas.drawLine(900F,30F,3000F,800F,paint2)
-
-
-
-
-
-//
-//        for (x in 0 until source.width) {
-//            for (y in 0 until source.height) {
-//
-//                //If it is a wall or a room then walkable set to false
-//                if ( source.getPixel(x,y).toColor() == Color.rgb(218,54,54).toColor())
-//
-//    //                            floorNode[x][y] = Floor.FloorNode(x,y,"#da3636", "id"+x+y, false, false)
-//                else//It is a hallway set walkable to true
-//    //                            floorNode[x][y] = Floor.FloorNode(x,y,"#da3636", "id"+x+y, true, false)
-//                    bitmap.setPixel(x, y, Color.rgb(255, 0, 0))
-//            }
-//        }
+        canvas.drawLine(900F,30F,3000F,800F,pathPaint)
 
 
         source.recycle()
 
         return bitmap
     }
+
+
 }
 
 
