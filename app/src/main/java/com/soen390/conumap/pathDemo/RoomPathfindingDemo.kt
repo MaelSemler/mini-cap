59f2eede01f1e7f6b6c3f8e02e3b009b14dae725
Andy <andypqvu@hotmail.com>
2020-04-11 23:26:05 -0400

    Updated nested classes in Floor to declare properties and updatd RoomPathfindingDemo to print map to console.

--- app/src/main/java/com/soen390/conumap/pathDemo/RoomPathfindingDemo.kt
+++ app/src/main/java/com/soen390/conumap/pathDemo/RoomPathfindingDemo.kt
@@ -8,17 +8,29 @@ import com.soen390.conumap.building.Floor
 
 fun main (args: Array<String>) {
     var origin = Node (0,0)
-    var destination = Node (10, 10)
+    var destination = Node (0, 1)
     var floorMap: Floor.FloorPlan
 
     FloorCreator.createFloors()
 
+    //Temporarily hardcoded to first index since that's where H9 floor plan is currently stored
     floorMap = FloorCreator.floors[0].floorPlan
 
-    var pathfinding: Pathfinding = Pathfinding(17, 19, origin, destination)
+    for (array in floorMap.floorNodes) {
+        for (value in array) {
+            if (value.walkable == true) {
+                print ("-   ")
+            } else if (value.walkable == false) {
+                print("X   ")
+            }
+        }
+        println()
+    }
+
+    var pathfinding: Pathfinding = Pathfinding(floorMap.floorNodes.size,floorMap.floorNodes[0].size, origin, destination)
 
     pathfinding.loadMap()
     pathfinding.printMapSizeToConsole()
-    pathfinding.printMapToConsole()
+    //pathfinding.printMapToConsole()
 
 }
