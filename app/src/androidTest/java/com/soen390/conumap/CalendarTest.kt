b337a4a4655b4ace7875722df2f2557f3a3c5882
David Semujanga <dav.semu@gmail.com>
2020-04-13 13:23:02 -0400

    Added System tests to the Calendar

--- app/src/androidTest/java/com/soen390/conumap/CalendarTest.kt
+++ app/src/androidTest/java/com/soen390/conumap/CalendarTest.kt
@@ -5,23 +5,22 @@ import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiObject
 import androidx.test.uiautomator.UiSelector
+import org.junit.Assert.assertEquals
 
-import org.junit.Before
 import org.junit.Test
-import org.junit.After
 import org.junit.runner.RunWith
 
 import java.lang.Thread.sleep
+import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 class CalendarTest {
     var device = UiDevice.getInstance(getInstrumentation())
     var height = device.displayHeight
     var width = device.displayWidth
-/*
+    //Your Google account must already be connected for this test to pass
     // SYSTEM TEST STX-X.
-    @Test
-    fun signInTest() {
+    fun reachCalendar(){
         // Open app.
         device.pressRecentApps()
 
@@ -35,8 +34,6 @@ class CalendarTest {
         // Give time for app to load.
         sleep(5000)
 
-
-
         val menuButton: UiObject = device.findObject(
             UiSelector().descriptionMatches("Open navigation drawer")
         )
@@ -47,63 +44,78 @@ class CalendarTest {
         )
         val calendarItem = parentItem.getChild(UiSelector().index(2))
         calendarItem.click()
-
-        val signInButton: UiObject = device.findObject(
-            UiSelector().resourceId("com.soen390.conumap:id/sign_in_button")
+        //Time for the calendar page to load
+        sleep(5000)
+    }
+    @Test
+    fun signInTest() {
+        reachCalendar()
+        val signOutButton: UiObject = device.findObject(
+            UiSelector().resourceId("com.soen390.conumap:id/debug_sign_out")
         )
-        signInButton.click()
-
-
-        //Have some sort of confirmation that the "Choose an account" window pops up
-        /*
-        * if sign in button found, stop test
-        * if not,
-        * see that */
+        assertEquals(signOutButton.isClickable, true)
     }
 
     @Test
-    fun signedInTest() {
-        // Open app.
-        device.pressRecentApps()
-
-        sleep(1000)
-
-        device.click(
-            width / 2,
-            height / 2
+    fun comingUpTest() {
+        reachCalendar()
+        val classNumberText: UiObject = device.findObject(
+            UiSelector().resourceId("com.soen390.conumap:id/class_number_value")
         )
+        assertEquals(classNumberText.text != "Class Number", true)
 
-        // Give time for app to load.
-        sleep(5000)
-
-
-
-        val menuButton: UiObject = device.findObject(
-            UiSelector().descriptionMatches("Open navigation drawer")
+        val timeText: UiObject = device.findObject(
+            UiSelector().resourceId("com.soen390.conumap:id/time_value")
         )
-        menuButton.click()
+        assertEquals(timeText.text != "Time value", true)
 
-        val parentItem: UiObject = device.findObject(
-            UiSelector().resourceId("com.soen390.conumap:id/design_navigation_view")
+        val roomText: UiObject = device.findObject(
+            UiSelector().resourceId("com.soen390.conumap:id/room_location_value")
         )
-        val calendarItem = parentItem.getChild(UiSelector().index(2))
-        calendarItem.click()
+        assertEquals(roomText.text != "Room Location value", true)
 
-        //assert() the sign in button is there
-        //if it's there end all tests
-        //if not assert that the go to button is there
-        val signInButton: UiObject = device.findObject(
-            UiSelector().resourceId("com.soen390.conumap:id/sign_in_button")
+        val locationText: UiObject = device.findObject(
+            UiSelector().resourceId("com.soen390.conumap:id/location_value")
         )
-        signInButton.click()
+        assertEquals(locationText.text != "Location value", true)
+    }
 
+    @Test
+    fun dropdownTest() {
+        reachCalendar()
+        val dropdown: UiObject = device.findObject(
+            UiSelector().resourceId("com.soen390.conumap:id/dropdown_calendar")
+        )
+        //if the dropdown is initialised, then it is scrollable
+        assertEquals(dropdown.isScrollable,true)
+    }
+    @Test
+    fun calendarTest() {
+        reachCalendar()
+        val calendarText: UiObject = device.findObject(
+            UiSelector().resourceId("com.soen390.conumap:id/month_year")
+        )
+        assertEquals(calendarText.text != "Month Year",true)
 
-        //Have some sort of confirmation that the "Choose an account" window pops up
-        /*
-        * if sign in button found, stop test
-        * if not,
-        * see that */
-    }*/
-
-
+        val date = java.util.Calendar.getInstance()
+        val month = date.getDisplayName(java.util.Calendar.MONTH,java.util.Calendar.LONG, Locale.getDefault())
+        val year = date.get(java.util.Calendar.YEAR).toString()
+        val dateStrings = calendarText.text.split(" ")
+        //there are three possible date formats
+        // Month Year, MonthA - MonthB Year, MonthA YearA - MonthB YearB
+        when (dateStrings.size) {
+            2 -> {
+                assertEquals(dateStrings[0] == month,true)
+                assertEquals(dateStrings[1] == year,true)
+            }
+            4 -> {
+                assertEquals(dateStrings[0] == month || dateStrings[2] == month,true)
+                assertEquals(dateStrings[3] == year,true)
+            }
+            5 -> {
+                assertEquals(dateStrings[0] == month || dateStrings[3] == month,true)
+                assertEquals(dateStrings[1] == year || dateStrings[4] == month,true)
+            }
+        }
+    }
 }
