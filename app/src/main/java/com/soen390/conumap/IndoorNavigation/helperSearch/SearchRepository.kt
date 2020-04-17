951b487e4542b396dd7b4b5727e3d684af821402
nguyhh <nguyenhaiha1224@gmail.com>
2020-04-17 15:02:04 -0400

    Attempt at retrieving results from DB

--- app/src/main/java/com/soen390/conumap/IndoorNavigation/helperSearch/SearchRepository.kt
+++ app/src/main/java/com/soen390/conumap/IndoorNavigation/helperSearch/SearchRepository.kt
@@ -1,6 +1,8 @@
 package com.soen390.conumap.IndoorNavigation.helperSearch
 
 import android.content.res.AssetManager
+import android.util.Log
+import com.soen390.conumap.IndoorNavigation.IndoorDatabaseHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.BufferedReader
@@ -16,6 +18,7 @@ class SearchRepository (private val assets: AssetManager,
     companion object {
         private const val DEFAULT_RESULT_MAX_SIZE = 250
         private const val RANDOM_ERROR_THRESHOLD = 0.75
+        lateinit var db: IndoorDatabaseHelper
     }
 
     override suspend fun performSearch(query: String): List<String> {
@@ -27,16 +30,28 @@ class SearchRepository (private val assets: AssetManager,
             }
 
             //TODO: Connect with Database
-            println("Search for $query")
-            val inputStream = assets.open("ClassRoom.txt")
-            val inputStreamReader = InputStreamReader(inputStream)
-            val bufferedReader = BufferedReader(inputStreamReader)
-            bufferedReader.use { reader: BufferedReader ->
-                reader.lineSequence()
-                    .filter { it.contains(query, true) }
-                    .take(maxResult)
-                    .toList()
-            }
+            val listRooms =db.getAllRoomCodes()
+                .filter{it.contains(query,true)}
+                .take(maxResult)
+                .toList()
+
+            Log.i("TESTING:" ,listRooms[0])
+
+            return@withContext listRooms
+//            println("Search for $query")
+//            val inputStream = assets.open("ClassRoom.txt")
+//            val inputStreamReader = InputStreamReader(inputStream)
+//
+//            val bufferedReader = BufferedReader(inputStreamReader)
+//            bufferedReader.use { reader: BufferedReader ->
+//                reader.lineSequence()
+//                    .filter { it.contains(query, true) }
+//                    .take(maxResult)
+//                    .toList()
+//            }
+
+
+
         }
     }
 }
